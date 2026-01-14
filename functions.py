6f52bfc0563ccbbe907fcc29abf7629f4b3b68d2
kelajaksoati <kelajaksoati@gmail.com>
2026-01-14 10:39:26 +0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,7 +1,8 @@
-def calculate_teacher_salary(stavka, soat, sinf_rahbar_bhm, bhm):
-    # 18 soat = 1 stavka
-    dars_uchun = (stavka / 18) * soat
-    jami = dars_uchun + sinf_rahbar_bhm
-    # 12% daromad solig'i + 1% INPS
+def calculate_salary_logic(stavka, soat, sinf_rahbar_bhm_foiz, bhm):
+    # O'zbekiston tizimida 18 soat - 1 stavka
+    asosiy_oylik = (stavka / 18) * soat
+    sinf_uchun = bhm * (sinf_rahbar_bhm_foiz / 100)
+    jami = asosiy_oylik + sinf_uchun
+    # 12% daromad solig'i + 1% pensiya
     toza_oylik = jami * 0.87
     return round(toza_oylik, 2)
