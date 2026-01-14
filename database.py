f069dd4522b043dd69998e36174743aef0081864
kelajaksoati <kelajaksoati@gmail.com>
2026-01-14 10:32:43 +0500

    Update database.py

--- database.py
+++ database.py
@@ -4,15 +4,24 @@ class Database:
     def __init__(self, db_file):
         self.connection = sqlite3.connect(db_file)
         self.cursor = self.connection.cursor()
-        self.setup()
+        self.create_tables()
 
-    def setup(self):
+    def create_tables(self):
         with self.connection:
             self.cursor.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY)")
             self.cursor.execute("CREATE TABLE IF NOT EXISTS files (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, file_id TEXT, category TEXT, subject TEXT)")
             self.cursor.execute("CREATE TABLE IF NOT EXISTS settings (key TEXT PRIMARY KEY, value REAL)")
-            # BHM va stavkalarni kiritish
-            self.cursor.execute("INSERT OR IGNORE INTO settings VALUES ('bhm', 375000)")
+            # Standart stavkalarni kiritish
+            default_settings = [('bhm', 375000), ('oliy', 5000000), ('birinchi', 4500000), ('ikkinchi', 4000000), ('mutaxassis', 3500000)]
+            self.cursor.executemany("INSERT OR IGNORE INTO settings VALUES (?, ?)", default_settings)
+
+    def add_user(self, user_id):
+        with self.connection:
+            return self.cursor.execute("INSERT OR IGNORE INTO users (id) VALUES (?)", (user_id,))
+
+    def get_users(self):
+        with self.connection:
+            return self.cursor.execute("SELECT id FROM users").fetchall()
 
     def add_file(self, name, file_id, category, subject):
         with self.connection:
@@ -21,3 +30,16 @@ class Database:
     def get_files(self, category, subject):
         with self.connection:
             return self.cursor.execute("SELECT name, file_id FROM files WHERE category=? AND subject=?", (category, subject)).fetchall()
+
+    def update_setting(self, key, value):
+        with self.connection:
+            return self.cursor.execute("UPDATE settings SET value=? WHERE key=?", (value, key))
+
+    def get_setting(self, key):
+        with self.connection:
+            res = self.cursor.execute("SELECT value FROM settings WHERE key=?", (key,)).fetchone()
+            return res[0] if res else 0
+
+    def clean_users(self):
+        with self.connection:
+            return self.cursor.execute("DELETE FROM users")
