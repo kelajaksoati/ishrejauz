50e4850fa6b94401f9f4b261874a91178478861e
kelajaksoati <kelajaksoati@gmail.com>
2026-01-14 11:11:25 +0500

    Update config.py

--- config.py
+++ config.py
@@ -5,3 +5,8 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = int(os.getenv("ADMIN_ID"))
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+# Kategoriyalar
+CATEGORIES = ["📚 Ish rejalar", "📝 Testlar", "📁 Darsliklar", "📢 Vakansiyalar"]
+SUBJECTS = ["Ona tili", "Matematika", "Ingliz tili", "Tarix", "Fizika", "Biologiya"]
