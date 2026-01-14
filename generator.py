52ff122ba906f3b381c26764c16f3d90e2bb4a92
kelajaksoati <kelajaksoati@gmail.com>
2026-01-14 11:12:51 +0500

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,15 +1,16 @@
-from reportlab.lib.pagesizes import A4
 from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import A4
 import io
 
-def generate_tavsifnoma(name, school):
+def generate_certificate_pdf(name, score):
     packet = io.BytesIO()
     can = canvas.Canvas(packet, pagesize=A4)
-    can.setFont("Helvetica-Bold", 20)
-    can.drawString(200, 750, "TAVSIFNOMA")
-    can.setFont("Helvetica", 14)
-    text = f"Ushbu tavsifnoma {school} maktabi o'qituvchisi {name}ga berildi..."
-    can.drawString(50, 700, text)
+    # Dizayn qismi
+    can.setFont("Helvetica-Bold", 30)
+    can.drawCentredString(297, 600, "SERTIFIKAT")
+    can.setFont("Helvetica", 20)
+    can.drawCentredString(297, 500, f"{name}")
+    can.drawCentredString(297, 450, f"Ball: {score}")
     can.save()
     packet.seek(0)
     return packet
