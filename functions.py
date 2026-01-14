8cc7c480cf27f44e63e5bbe183bf918c42d89e0d
kelajaksoati <kelajaksoati@gmail.com>
2026-01-14 11:02:17 +0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,8 +1,14 @@
-def calculate_salary_logic(stavka, soat, sinf_rahbar_bhm_foiz, bhm):
-    # O'zbekiston tizimida 18 soat - 1 stavka
-    asosiy_oylik = (stavka / 18) * soat
-    sinf_uchun = bhm * (sinf_rahbar_bhm_foiz / 100)
-    jami = asosiy_oylik + sinf_uchun
-    # 12% daromad solig'i + 1% pensiya
-    toza_oylik = jami * 0.87
+def calculate_salary_logic(stavka, soat, sinf_foiz, bhm):
+    """
+    Oylik hisoblash formulasi:
+    1. Dars soati uchun: (Stavka / 18) * soat
+    2. Sinf rahbarlik uchun: (BHM * foiz / 100)
+    3. Soliqlar: Jami summadan 13% ayiriladi (12% daromad + 1% INPS)
+    """
+    dars_uchun = (stavka / 18) * soat
+    sinf_uchun = bhm * (sinf_foiz / 100)
+    
+    jami = dars_uchun + sinf_uchun
+    toza_oylik = jami * 0.87  # 100% - 13% = 87%
+    
     return round(toza_oylik, 2)
